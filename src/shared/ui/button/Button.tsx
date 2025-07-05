d2b89e10de273664edd28cd22ad2e64e53eb59e7
sli9 <sil9gosy@mail.ru>
2025-07-05 17:21:17 +0300

    fix: add styles for button

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -1,16 +1,32 @@
 import type { ComponentPropsWithRef, ElementType, ReactElement, ReactNode } from "react"
+import s from "./Button.module.css"
 
 type ButtonProps<T extends ElementType> = {
   children?: ReactNode
   className?: string
   as?: T
+  variant?: "rounded" | "square"
+  size?: "sm" | "md" | "lg"
 } & Omit<ComponentPropsWithRef<T>, "as">
 
-export function Button<T extends ElementType>({ className, as, children, ...rest }: ButtonProps<T>): ReactElement {
+export function Button<T extends ElementType>({
+  className,
+  as,
+  children,
+  variant = "square",
+  size = "md",
+  ...rest
+}: ButtonProps<T>): ReactElement {
   const Component = as || "button"
 
+  const classNames = ` 
+  ${s.button}
+  ${s[size]}
+  ${s[variant]}
+  ${className}`
+
   return (
-    <Component className={className} {...rest}>
+    <Component className={classNames} {...rest}>
       {children}
     </Component>
   )
