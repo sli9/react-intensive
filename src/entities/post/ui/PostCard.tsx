2ff8d1f6dcef77a1a339b8e5c763410dd3c10bd6
sli9 <sil9gosy@mail.ru>
2025-07-10 13:51:55 +0300

    feature: add comment list

--- src/entities/post/ui/PostCard.tsx
+++ src/entities/post/ui/PostCard.tsx
@@ -1,4 +1,3 @@
-import s from "./PostCard.module.css"
 import type { Post } from "@/widgets/postList"
 
 type PostCardProps = {
@@ -7,12 +6,12 @@ type PostCardProps = {
 
 export const PostCard = ({ post }: Props) => {
   return (
-    <div className={s.card}>
+    <>
       <div>
         <p>User ID: {post.id}</p>
       </div>
       <h3>{post.title}</h3>
       <p>{post.body}</p>
-    </div>
+    </>
   )
 }
---
+++ src/shared/mocks/mockComments.ts
@@ -0,0 +1,43 @@
+import type { MockComments } from "@/widgets/commentList"
+
+export const mockComments: MockComments = {
+  1: [
+    {
+      id: 1,
+      text: "Great post! I really enjoyed reading it.",
+      author: "John Doe",
+    },
+    {
+      id: 2,
+      text: "Could you elaborate more on this topic?",
+      author: "Jane Smith",
+    },
+    {
+      id: 3,
+      text: "Could you elaborate more on this topic?",
+      author: "Jane Smith",
+    },
+    {
+      id: 4,
+      text: "Could you elaborate more on this topic?",
+      author: "Jane Smith",
+    },
+    {
+      id: 5,
+      text: "Could you elaborate more on this topic?",
+      author: "Jane Smith",
+    },
+  ],
+  2: [
+    {
+      id: 3,
+      text: "This is really helpful information!",
+      author: "Mike Johnson",
+    },
+    {
+      id: 4,
+      text: "I have a question about this...",
+      author: "Sarah Wilson",
+    },
+  ],
+}
