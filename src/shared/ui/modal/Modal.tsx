f57edd8c6abdb4835b963f95b89fd255b0201f6b
sli9 <sil9gosy@mail.ru>
2025-07-11 22:50:45 +0300

    fix: change condition in the useTheme and delete Fragment in the Modal

--- src/shared/ui/modal/Modal.tsx
+++ src/shared/ui/modal/Modal.tsx
@@ -30,24 +30,19 @@ export const Modal = ({ onClose, open, modalTitle, children }: ModalProps) => {
   }, [open, onClose])
 
   return (
-    <>
-      {open && (
-        <>
-          {createPortal(
-            <div className={s.overlay}>
-              <div className={s.content}>
-                <h2 className={s.title}>{modalTitle}</h2>
-                <hr />
-                {children}
-                <Button className={s.closeIcon} variant="rounded" size="sm" onClick={onClose}>
-                  x
-                </Button>
-              </div>
-            </div>,
-            document.body
-          )}
-        </>
-      )}
-    </>
+    open &&
+    createPortal(
+      <div className={s.overlay}>
+        <div className={s.content}>
+          <h2 className={s.title}>{modalTitle}</h2>
+          <hr />
+          {children}
+          <Button className={s.closeIcon} variant="rounded" size="sm" onClick={onClose}>
+            x
+          </Button>
+        </div>
+      </div>,
+      document.body
+    )
   )
 }
