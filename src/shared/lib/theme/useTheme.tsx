b46389ac9d901197b6c0f2e6f50c810230e0be10
sli9 <sil9gosy@mail.ru>
2025-07-11 22:50:45 +0300

    fix: change condition in the useTheme

--- src/shared/lib/theme/useTheme.tsx
+++ src/shared/lib/theme/useTheme.tsx
@@ -3,7 +3,7 @@ import { ThemeContext } from "./ThemeProvider"
 
 export const useTheme = () => {
   const context = useContext(ThemeContext)
-  if (context === undefined) {
+  if (!context) {
     throw new Error("useTheme must be used within a ThemeProvider")
   }
   return context
